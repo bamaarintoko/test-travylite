798972fea973b84577d8d0c31d47a0e7707337c0
bama <yosafatbama.arintoko@gmail.com>
2021-11-29 15:19:15 +0700

    done

--- pages/index.js
+++ pages/index.js
@@ -21,11 +21,6 @@ const menuArr = [
 export default function Home() {
 	return (
 		<Container maxWidth="md" style={{ paddingLeft: 0, paddingRight: 0 }}>
-			<Head>
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-				<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
-			</Head>
 			<Grid container spacing={0}>
 				<Grid item md={12} xs={12} className={styles.header_home}>
 					{/* <Item>xs=6 md=8</Item> */}
