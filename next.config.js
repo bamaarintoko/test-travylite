14983e52b01e7da8389a3024f811273299899b1d
bama <yosafatbama.arintoko@gmail.com>
2021-11-30 12:10:05 +0700

    done

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 module.exports = withPWA({
-  // pwa: {
-  //   dest: "public",
-  //   register: true,
-  //   skipWaiting: true,
-  // },
   pwa: {
-    dest: 'public',
-    publicExcludes: [
-        '!robots.txt',
-        '!sitemap.xml.gz',
-    ],
-},
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+  //   pwa: {
+  //     dest: 'public',
+  //     publicExcludes: [
+  //         '!robots.txt',
+  //         '!sitemap.xml.gz',
+  //     ],
+  // },
 });
 
 
