fecbbd9690e2284a1e1ce2d44407f9ea01b3da5e
bama <yosafatbama.arintoko@gmail.com>
2021-11-30 15:49:18 +0700

    done

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,7 @@ class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
-                    <link rel="apple-touch-icon" href="/icon_192.png"></link>
+                    <link rel="apple-touch-icon" href="/icon-192x192.png"></link>
                     <meta name="theme-color" content="#fff" />
                 </Head>
                 <body>
