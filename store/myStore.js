bcdc468b057d97035fec3eb3b74037c4defd26c6
bama <yosafatbama.arintoko@gmail.com>
2022-02-21 17:36:56 +0700

    change env

--- store/myStore.js
+++ store/myStore.js
@@ -1,10 +1,19 @@
-import { combineReducers, applyMiddleware, createStore } from 'redux';
+import { combineReducers, applyMiddleware, createStore, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk'
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import { defaultReducer } from '../reducer/defaultReducer'
+
+const middlewares = [];
+if (process.env.NODE_ENV === `development`) {
+    const { logger } = require(`redux-logger`);
+  
+    middlewares.push(logger);
+  }
 export const appReducer = combineReducers({
     defaultReducer,
 })
 
-export const myStore = createStore(appReducer, applyMiddleware(thunkMiddleware, logger))
+export const myStore = compose(applyMiddleware(...middlewares))(createStore)(appReducer);
+
+// export const myStore = createStore(appReducer, applyMiddleware(thunkMiddleware, logger))
 
